b076896d51b8398f3884aa2b8f6162ce2e5dfaee
Aenze <smileyrence3@gmail.com>
2025-09-29 22:17:30 +0800

    added tags and create_at, and filter for tags

--- notes/models.py
+++ notes/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.utils.timezone import now
+
 
 # Create your models here.
 class Notes(models.Model):
     title = models.CharField(max_length=100)
+    tags = models.CharField(max_length=50, blank=True, null=True)
     content = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
